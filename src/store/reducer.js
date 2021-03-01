358da976c112f622b3c87ff11c65db5cb470f8be
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-03-01 16:07:45 -0500

    created store

---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  secondPass: "",
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
