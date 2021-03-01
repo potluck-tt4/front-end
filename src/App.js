7edae304e526aee64a4332705e6c83fcf00b5b74
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-03-01 12:48:55 -0500

    Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Button from "./components/button/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button label="Click me please!"></button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
